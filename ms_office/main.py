d5767a0ddb854ab9be7c2506dc01d4db6c1bee23
nazmulmilon <milonsarker73@gmail.com>
2022-04-07 12:27:00 +0600

    editor maximize and big font size

--- ms_office/main.py
+++ ms_office/main.py
@@ -8,6 +8,8 @@ class MSWord(QMainWindow):
         super(MSWord, self).__init__()
         self.editor = QTextEdit()
         self.setCentralWidget(self.editor)
+        self.showMaximized()
+        self.editor.setFontPointSize(20)
 
 
 app = QApplication(sys.argv)
